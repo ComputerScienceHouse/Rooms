ef9c44e7a990dffda5e7d33751d99e02e93eaf45
Lonnie Gerol <lonnie@lonniegerol.com>
2021-02-21 02:30:12 -0500

    imports routes module in app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@
 The Primary entry point for the Flask backend.
 """
 from . import APP
+from . import routes
 
 if __name__ == '__main__':
     APP.run(host=APP.config['IP'], port=int(APP.config['PORT']))
