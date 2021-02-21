375c6aabcdfcfd2d27b2d5be6f21662e14a6d1dc
Lonnie Gerol <lonnie@lonniegerol.com>
2021-02-20 19:38:09 -0500

    Added newline to end-of-file

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ The Primary entry point for the Flask backend.
 from server import APP
 
 if __name__ == "__main__":
-    APP.run(host=APP.config["IP"], port=int(APP.config["PORT"]))
+    APP.run(host=APP.config["IP"], port=int(APP.config["PORT"])
