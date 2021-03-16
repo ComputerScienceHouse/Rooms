ad1e241f6716cfc0b888781b24b14e052a3ca226
Lonnie Gerol <lonnie@lonniegerol.com>
2021-03-16 02:52:23 -0400

    Add DB enviorment variables to config.env.py

--- backend/config.env.py
+++ backend/config.env.py
@@ -5,3 +5,6 @@ from os import environ as env
 # LDAP
 LDAP_BIND_DN = env.get("ROOMS_LDAP_BIND_DN", "")
 LDAP_BIND_PW = env.get("ROOMS_LDAP_BIND_PW", "")
+
+# DB Info
+SQLALCHEMY_DATABASE_URI = env.get("SQLALCHEMY_DATABASE_URI", "postgresql://postgres:mysecretpassword@localhost:5432/postgres")
